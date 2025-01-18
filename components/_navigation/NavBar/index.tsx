60a00f3c3e2ce0a207380ec15aa0fe6429e756dd
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 14:16:08 -0700

    Rendered logo image

--- components/_navigation/NavBar/index.tsx
+++ components/_navigation/NavBar/index.tsx
@@ -1,14 +1,27 @@
 import { use } from 'react'
 import { getMainMenu } from '@/lib/butter'
 import { headers } from 'next/headers';
+import Image from 'next/image'
 
 const NavBar = () => {
     const headersList = use(headers());
     const isPreview = headersList.get("x-search-param")
     const navContent = use(getMainMenu(isPreview as string))
-    console.log('navContent', navContent)
+    const {
+        fields
+    } = navContent?.data
+
+    console.log('nav fields', fields)
+
     return (
-        <nav></nav>
+        <nav>
+            <Image
+                src={fields?.logo?.url}
+                alt={fields?.logo?.alt}
+                width={fields?.logo_width}
+                height={fields?.logo_height}
+            />
+        </nav>
     )
 }
 
