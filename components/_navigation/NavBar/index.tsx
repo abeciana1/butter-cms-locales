f0dfcc78ba9251daf3b8d45e68930bb33ecb8215
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 15:28:23 -0700

    Added flex styling and rendering nav link comps

--- components/_navigation/NavBar/index.tsx
+++ components/_navigation/NavBar/index.tsx
@@ -2,6 +2,8 @@ import { use } from 'react'
 import { getMainMenu } from '@/lib/butter'
 import { headers } from 'next/headers';
 import Image from 'next/image'
+import ComponentRenderer from '@/components/ComponentRender'
+import { NavigationLinkI } from '@/definitions/interfaces/_navigation'
 
 const NavBar = () => {
     const headersList = use(headers());
@@ -10,17 +12,27 @@ const NavBar = () => {
     const {
         fields
     } = navContent?.data
-
-    console.log('nav fields', fields)
-
     return (
-        <nav>
+        <nav className='flex relative justify-between items-center'>
             <Image
                 src={fields?.logo?.url}
                 alt={fields?.logo?.alt}
                 width={fields?.logo_width}
                 height={fields?.logo_height}
             />
+            <ul
+                className='flex gap-7 relative items-center'
+            >
+                {fields?.links?.map(({type, fields: sectionData}: NavigationLinkI, index: number) => {
+                    return (
+                        <ComponentRenderer
+                            key={type + index}
+                            type={type}
+                            sectionData={sectionData}
+                        />
+                    )
+                })}
+            </ul>
         </nav>
     )
 }
