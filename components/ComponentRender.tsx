27aa13bfe771718fc484aa10932d32bb6a56e4ba
Alex Beciana <alex.beciana@gmail.com>
2025-01-17 23:43:36 -0700

    Created dynamic rendering component

---
+++ components/ComponentRender.tsx
@@ -0,0 +1,27 @@
+import dynamic from "next/dynamic"
+import { toPascalCase } from "@/lib/helper-functions"
+import camelcaseKeys from 'camelcase-keys';
+import {
+    type ComponentRendererI,
+    type MissingComponentI
+} from '@/definitions/interfaces/general'
+
+const MissingComponent: React.FC<MissingComponentI> = ({ type }) => {
+    return (
+        <div>Missing component: {toPascalCase(type)}</div>
+    )
+}
+
+const ComponentRenderer = ({ type, sectionData }: ComponentRendererI) => {
+	const sectionsComponentPaths = () => ({
+		[type]: dynamic(
+			() => import(`@/components/${toPascalCase(type)}`)
+				.catch(() => () => <MissingComponent type={type} />), {
+            ssr: true
+		})
+	});
+	const SectionComponent = sectionsComponentPaths()[type] || <MissingComponent type={type} />;
+	return <SectionComponent key={type+Date.now()} type={type} {...camelcaseKeys(sectionData)} />
+}
+
+export default ComponentRenderer
