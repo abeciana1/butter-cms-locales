d32fe9e8e8d48114c7875450a08c79093d72d844
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 15:30:45 -0700

    Created LinkI interface

--- definitions/interfaces/_styled/index.ts
+++ definitions/interfaces/_styled/index.ts
@@ -1,6 +1,20 @@
+import {
+    ColorE,
+    FontWeightE,
+    FontSizeE
+} from '@/definitions/enums'
+
 export interface ImageI {
     src: string;
     alt: string;
     width: number;
     height: number;
+}
+
+export interface LinkI {
+    href: string;
+    linkText: string;
+    color: ColorE;
+    fontWeight: FontWeightE;
+    fontSize: FontSizeE;
 }
