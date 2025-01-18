d36b389ca3b337f36eb3dee556d7d1f347662442
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 15:30:19 -0700

    Created enums for colors and fontweight and font size

---
+++ definitions/enums/index.ts
@@ -0,0 +1,21 @@
+export enum ColorE {
+    NAVY = 'NAVY',
+    WHITE = 'WHITE',
+    GREY = 'GREY',
+    DARKGREY = 'DARKGREY',
+    BLUE = 'BLUE',
+    LIGHTBLUE = 'LIGHTBLUE'
+}
+
+export enum FontWeightE {
+    LIGHT = 'LIGHT',
+    NORMAL = 'NORMAL',
+    MEDIUM = 'MEDIUM',
+    BOLD = 'BOLD'
+}
+
+export enum FontSizeE {
+    LG = 'LG',
+    XL = 'XL',
+    XXL = 'XXL'
+}
