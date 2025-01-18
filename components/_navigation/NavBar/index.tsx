6c71f85fa8615855de2a931275a929c47e89c33a
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 13:29:55 -0700

    Added fetch for nav content and check for preview header

--- components/_navigation/NavBar/index.tsx
+++ components/_navigation/NavBar/index.tsx
@@ -1,8 +1,12 @@
 import { use } from 'react'
 import { getMainMenu } from '@/lib/butter'
+import { headers } from 'next/headers';
 
 const NavBar = () => {
-    const navContent = getMainMenu()
+    const headersList = use(headers());
+    const isPreview = headersList.get("x-search-param")
+    console.log('isPreview', isPreview)
+    const navContent = getMainMenu(isPreview as string)
     console.log('navContent', navContent)
     return (
         <nav></nav>
