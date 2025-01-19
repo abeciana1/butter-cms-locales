d0516ad6364fe5a7cae3c546a3d38b2e1827ac04
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 23:23:23 -0700

    Adjusted styles for Hero component

--- components/_page-elements/SliderHero/index.tsx
+++ components/_page-elements/SliderHero/index.tsx
@@ -1,19 +1,54 @@
 import { HeroI, SliderHeroI } from '@/definitions/interfaces/_section'
 import Slider from '@/components/_styled/Slider'
 import Image from 'next/image'
+import { Heading1 } from '@/components/_styled/Heading'
+import { ColorE, FontWeightE, FontSizeE } from '@/definitions/enums'
+import TextContent from '@/components/_styled/TextContent'
+import cx from 'classnames'
+import ButtonLink from '@/components/_styled/ButtonLink'
 
 const Hero: React.FC<HeroI> = ({
     headline,
     text_body,
     cta_button_link,
     cta_button_label,
-    content_on_left,
+    content_on_right,
     background_image,
-    button_color
+    button_color,
+    headline_color,
+    headline_font_weight
 }) => {
-    console.log('background_image', background_image)
     return (
-        <section>
+        <section className='relative flex items-center'>
+            <div
+                className={cx('absolute flex flex-row z-50 top-5 lg:top-20 lg:w-1/2 pl-16', {
+                    ['right-0']: content_on_right
+                })}
+            >
+                <div className=''>
+                    <Heading1
+                        text={headline}
+                        fontWeight={headline_font_weight?.font_weight}
+                        color={headline_color?.color}
+                    />
+                    <div className='mt-5'>
+                        <TextContent
+                            text={text_body}
+                            color={headline_color?.color}
+                            fontSize={FontSizeE.XL}
+                            fontWeight={FontWeightE.MEDIUM}
+                        />
+                    </div>
+                    <div className='mt-5'>
+                        <ButtonLink
+                            link={cta_button_link}
+                            linkText={cta_button_label}
+                            buttonColor={button_color?.color}
+                        />
+                    </div>
+                </div>
+                <div/>
+            </div>
             <div className='relative'>
                 <Image
                     src={background_image?.url}
@@ -33,6 +68,7 @@ const SliderHero: React.FC<SliderHeroI> = ({
         <section>
             <Slider>
                 {hero?.map((hero: HeroI, index: number) => {
+                    console.log('hero', hero)
                     return (
                         <Hero
                             key={hero?.headline + index}
@@ -40,9 +76,11 @@ const SliderHero: React.FC<SliderHeroI> = ({
                             text_body={hero?.text_body}
                             cta_button_link={hero?.cta_button_link}
                             cta_button_label={hero?.cta_button_label}
-                            content_on_left={hero?.content_on_left}
+                            content_on_right={hero?.content_on_right}
                             background_image={hero?.background_image}
                             button_color={hero?.button_color}
+                            headline_color={hero?.headline_color}
+                            headline_font_weight={hero?.headline_font_weight}
                         />
                     )
                 })}
