605479b1c7bbf101168d865042530c4634c08d89
Alex Beciana <alex.beciana@gmail.com>
2025-01-19 16:29:23 -0700

    Added fontWeight prop and rendering cases

--- components/_styled/ButtonLink/index.tsx
+++ components/_styled/ButtonLink/index.tsx
@@ -1,4 +1,4 @@
-import { ColorE } from "@/definitions/enums";
+import { ColorE, FontWeightE } from "@/definitions/enums";
 import Link from 'next/link'
 import { ButtonLinkI } from '@/definitions/interfaces/_styled'
 import cx from 'classnames'
@@ -6,18 +6,23 @@ import cx from 'classnames'
 const ButtonLink: React.FC<ButtonLinkI> = ({
     linkText,
     buttonColor,
-    link
+    link,
+    fontWeight = FontWeightE.NORMAL
 }) => {
     return (
         <Link
             href={link || '/'}
-            className={cx('text-xl font-light py-1 px-7', {
+            className={cx('text-xl py-1 px-7', {
                 ['bg-navy text-white']: buttonColor === ColorE.NAVY,
                 ['bg-white text-navy']: buttonColor === ColorE.WHITE,
                 ['bg-grey text-navy']: buttonColor === ColorE.GREY,
                 ['bg-darkGrey text-white']: buttonColor === ColorE.DARKGREY,
                 ['bg-blue text-white']: buttonColor === ColorE.BLUE,
                 ['bg-blue-light text-navy']: buttonColor === ColorE.LIGHTBLUE,
+                ['font-light']: fontWeight === FontWeightE.LIGHT,
+                ['font-medium']: fontWeight === FontWeightE.MEDIUM,
+                ['font-normal']: fontWeight === FontWeightE.NORMAL,
+                ['font-bold']: fontWeight === FontWeightE.BOLD,
             })}
         >
             {linkText}
