b993e92b306f55d67173f2c05acee02ccb1fb522
Alex Beciana <alex.beciana@gmail.com>
2025-01-19 10:44:01 -0700

    Added Features component

--- components/_page-elements/Features/index.tsx
+++ components/_page-elements/Features/index.tsx
@@ -1,6 +1,26 @@
-const Features = () => {
+import { FeaturesSectionI, FeatureI } from '@/definitions/interfaces/_section'
+import Feature from '@/components/_styled/Feature'
+
+const Features: React.FC<FeaturesSectionI> = ({
+    feature
+}) => {
+    console.log('features props', feature)
     return (
-        <></>
+        <section
+            className='grid grid-cols-3 gap-16 mt-10 px-10'
+        >
+            {feature?.map((feature: FeatureI, index: number) => {
+                return (
+                    <Feature
+                        key={feature?.title + index}
+                        image={feature?.image}
+                        about={feature?.about}
+                        link={feature?.link}
+                        title={feature?.title}
+                    />
+                )
+            })}
+        </section>
     )
 }
 
