b60ae9372d445d7ef074cb479c4bdaea2e25a394
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 17:55:46 -0700

    Added rendering for SubNavigationLinks

--- components/_navigation/NavBar/index.tsx
+++ components/_navigation/NavBar/index.tsx
@@ -13,17 +13,32 @@ const NavBar = () => {
         fields
     } = navContent?.data as NavBarI
     return (
-        <nav className='flex relative justify-between items-center py-2 px-5'>
-            <Image
-                src={fields?.logo?.url}
-                alt={fields?.logo?.alt}
-                width={fields?.logo_width}
-                height={fields?.logo_height}
-            />
-            <ul
-                className='flex gap-7 items-center'
-            >
-                {fields?.links?.map(({type, fields: sectionData}: NavigationLinkI, index: number) => {
+        <>
+            <nav className='flex relative justify-between items-center py-2 px-5'>
+                <Image
+                    src={fields?.logo?.url}
+                    alt={fields?.logo?.alt}
+                    width={fields?.logo_width}
+                    height={fields?.logo_height}
+                />
+                <ul
+                    className='flex gap-7 items-center'
+                >
+                    {fields?.links?.map(({type, fields: sectionData}: NavigationLinkI, index: number) => {
+                        if (type === 'navigation_link') {
+                            return (
+                                <ComponentRenderer
+                                    key={type + index}
+                                    type={type}
+                                    sectionData={sectionData}
+                                />
+                            )
+                        }
+                    })}
+                </ul>
+            </nav>
+            {fields?.links?.map(({type, fields: sectionData}: NavigationLinkI, index: number) => {
+                if (type === 'sub_navigation_links') {
                     return (
                         <ComponentRenderer
                             key={type + index}
@@ -31,9 +46,9 @@ const NavBar = () => {
                             sectionData={sectionData}
                         />
                     )
-                })}
-            </ul>
-        </nav>
+                }
+            })}
+        </>
     )
 }
 
