50019a80f07eeae7f2696da569b052b8740c2e57
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 19:14:12 -0700

    Created SliderHero and Hero components

--- components/_page-elements/SliderHero/index.tsx
+++ components/_page-elements/SliderHero/index.tsx
@@ -1,6 +1,53 @@
-const SliderHero = () => {
+import { HeroI, SliderHeroI } from '@/definitions/interfaces/_section'
+import Slider from '@/components/_styled/Slider'
+import Image from 'next/image'
+
+const Hero: React.FC<HeroI> = ({
+    headline,
+    text_body,
+    cta_button_link,
+    cta_button_label,
+    content_on_left,
+    background_image,
+    button_color
+}) => {
+    console.log('background_image', background_image)
     return (
-        <section></section>
+        <section>
+            <div className='relative'>
+                <Image
+                    src={background_image?.url}
+                    alt={background_image.alt}
+                    width={1600}
+                    height={500}
+                />
+            </div>
+        </section>
+    )
+}
+
+const SliderHero: React.FC<SliderHeroI> = ({
+    hero
+}) => {
+    return (
+        <section>
+            <Slider>
+                {hero?.map((hero: HeroI, index: number) => {
+                    return (
+                        <Hero
+                            key={hero?.headline + index}
+                            headline={hero?.headline}
+                            text_body={hero?.text_body}
+                            cta_button_link={hero?.cta_button_link}
+                            cta_button_label={hero?.cta_button_label}
+                            content_on_left={hero?.content_on_left}
+                            background_image={hero?.background_image}
+                            button_color={hero?.button_color}
+                        />
+                    )
+                })}
+            </Slider>
+        </section>
     )
 }
 
