75d7dc830e877812114714823c74ceade29e8184
Alex Beciana <alex.beciana@gmail.com>
2025-01-18 17:10:58 -0700

    Created SubNavigationLinksI interface

--- definitions/interfaces/_navigation/index.ts
+++ definitions/interfaces/_navigation/index.ts
@@ -2,15 +2,22 @@ export interface NavigationLinkFieldI {
     label: string;
     url: string;
     subsection_links?: SubsectionLinkI[];
-    subsectionLinks?: SubsectionLinkI[]
+    subsectionLinks?: SubsectionLinkI[];
 }
 
-interface SubsectionLinkI {
+export interface SubsectionLinkI {
     label: string;
     url: string;
-    meta: {
+    meta?: {
         id: number;
     }
+}
+
+export interface SubNavigationLinksI {
+    label: string;
+    subsectionLinks: SubsectionLinkI[];
+    setDisplay: (arg0: boolean) => void;
+    setKeep: (arg0: boolean) => void;
 }
 
 export interface NavigationLinkI {
