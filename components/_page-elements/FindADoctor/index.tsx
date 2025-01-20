438a598741312f159365499c0db9778f79ab8ea0
Alex Beciana <alex.beciana@gmail.com>
2025-01-19 20:30:52 -0700

    Created Doctor components and rendered

--- components/_page-elements/FindADoctor/index.tsx
+++ components/_page-elements/FindADoctor/index.tsx
@@ -1,25 +1,138 @@
 import { use } from 'react'
 import { collectionDataFetch } from '@/lib/butter'
-import { FindADoctorI } from '@/definitions/interfaces/_section'
+import { FindADoctorI, DoctorCardI } from '@/definitions/interfaces/_section'
 import { Heading1 } from '@/components/_styled/Heading'
-import { ColorE, FontWeightE } from '@/definitions/enums'
+import { ColorE, FontSizeE, FontWeightE } from '@/definitions/enums'
 import { SectionSpacing } from '@/components/_layouts'
+import Image from 'next/image'
+import Link from 'next/link'
+import TextContent from '@/components/_styled/TextContent'
+
+const DoctorCard: React.FC<DoctorCardI> = ({
+    address,
+    city_and_state,
+    doc_page_link,
+    facility,
+    fax,
+    phone,
+    headshot,
+    map_link,
+    name,
+    zip_code
+}) => {
+    return (
+        <div className='relative border-2 border-grey p-8 max-w-[825px]'>
+            <div className='flex gap-5'>
+                <Image
+                    src={headshot?.url}
+                    alt={headshot?.alt}
+                    width={148}
+                    height={158}
+                    className='max-h-[158px]'
+                />
+                <div className='relative w-full'>
+                    <h2 className='text-3xl'>
+                        <Link
+                            href={doc_page_link}
+                            className='text-navy hover:text-blue'
+                        >
+                            {name}
+                        </Link>
+                    </h2>
+                    <hr className='border-[1px] border-grey my-5'/>
+                    <div className='flex flex-col text-right justify-between'>
+                        <div>
+                            <TextContent
+                                text={facility}
+                                color={ColorE.DARKGREY}
+                                fontWeight={FontWeightE.MEDIUM}
+                                fontSize={FontSizeE.LG}
+                            />
+                            <TextContent
+                                text={address}
+                                color={ColorE.DARKGREY}
+                                fontWeight={FontWeightE.MEDIUM}
+                                fontSize={FontSizeE.LG}
+                            />
+                            <TextContent
+                                text={city_and_state}
+                                color={ColorE.DARKGREY}
+                                fontWeight={FontWeightE.MEDIUM}
+                                fontSize={FontSizeE.LG}
+                            />
+                            <TextContent
+                                text={zip_code}
+                                color={ColorE.DARKGREY}
+                                fontWeight={FontWeightE.MEDIUM}
+                                fontSize={FontSizeE.LG}
+                            />
+                            <div className='flex gap-2 justify-end'>
+                                <span>Phone:</span>
+                                <TextContent
+                                    text={phone}
+                                    color={ColorE.DARKGREY}
+                                    fontWeight={FontWeightE.BOLD}
+                                    fontSize={FontSizeE.LG}
+                                />
+                            </div>
+                            <div className='flex gap-2 justify-end'>
+                                <span>Fax:</span>
+                                <TextContent
+                                    text={fax}
+                                    color={ColorE.DARKGREY}
+                                    fontWeight={FontWeightE.BOLD}
+                                    fontSize={FontSizeE.LG}
+                                />
+                            </div>
+                        </div>
+                        <div className='flex justify-end'>
+                            <Link
+                                href={map_link}
+                                className='text-blue hover:text-navy font-bold text-xl mt-8 max-w-fit text-right'
+                            >
+                                View Map
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const FindADoctor: React.FC<FindADoctorI> = ({
     title
 }) => {
-    const doctors = use(collectionDataFetch('doctor'))
-    console.log('doctors', doctors)
+    const { doctor } = use(collectionDataFetch('doctor'))
     return (
-        <section className='mt-20'>
+        <ul className='mt-20'>
             <SectionSpacing>
                 <Heading1
                     text={title}
                     color={ColorE.NAVY}
                     fontWeight={FontWeightE?.NORMAL}
                 />
+                <section className='mt-9 space-y-10'>
+                    {doctor?.map((doc: DoctorCardI) => {
+                        return (
+                            <DoctorCard
+                                key={doc?.meta?.id}
+                                address={doc?.address}
+                                city_and_state={doc?.city_and_state}
+                                facility={doc?.facility}
+                                fax={doc?.fax}
+                                headshot={doc?.headshot}
+                                map_link={doc?.map_link}
+                                name={doc?.name}
+                                phone={doc?.phone}
+                                zip_code={doc?.zip_code}
+                                doc_page_link={doc?.doc_page_link}
+                            />
+                        )
+                    })}
+                </section>
             </SectionSpacing>
-        </section>
+        </ul>
     )
 }
 
