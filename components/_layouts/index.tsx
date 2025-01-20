0aea5a7ba39d73a4ff3beb4790f85fed4dead654
Alex Beciana <alex.beciana@gmail.com>
2025-01-19 17:35:47 -0700

    Created SectionSpacing component

--- components/_layouts/index.tsx
+++ components/_layouts/index.tsx
@@ -7,3 +7,11 @@ export const PageMarginWrapper: React.FC<PageMarginI> = ({ children }) => {
         </section>
     )
 }
+
+export const SectionSpacing: React.FC<PageMarginI> = ({ children }) => {
+    return (
+        <section className='relative mx-auto px-5'>
+            {children}
+        </section>
+    )
+}
