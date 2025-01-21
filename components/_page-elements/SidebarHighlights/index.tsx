b0af75446947372451a83953a7c2f3cfbbbc5d6e
Alex Beciana <alex.beciana@gmail.com>
2025-01-20 23:36:33 -0700

    Created initial render of SidebarHighlights component

---
+++ components/_page-elements/SidebarHighlights/index.tsx
@@ -0,0 +1,12 @@
+import { SidebarHighlightsI } from '@/definitions/interfaces/_sidebar'
+
+const SidebarHighlights: React.FC<SidebarHighlightsI> = ({
+    highlights
+}) => {
+    console.log({ highlights })
+    return (
+        <div></div>
+    )
+}
+
+export default SidebarHighlights
