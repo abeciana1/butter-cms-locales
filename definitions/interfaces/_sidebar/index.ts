206425192663157305c73c1542b1ba3b8cd8e85c
Alex Beciana <alex.beciana@gmail.com>
2025-01-20 20:22:36 -0700

    Created Sidebar interfaces

--- definitions/interfaces/_sidebar/index.ts
+++ definitions/interfaces/_sidebar/index.ts
@@ -1,3 +1,5 @@
+import { ButterImage } from '@/definitions/interfaces/general'
+
 export interface SidebarLinkI {
     link: string;
     link_text: string;
@@ -6,4 +8,36 @@ export interface SidebarLinkI {
 export interface SidebarLinksI {
     header: string;
     links: SidebarLinkI[];
+}
+
+export interface SidebarTabI {
+    sidebar_icon: ButterImage;
+    reference_collection: boolean;
+    reference_dropdown: string;
+    limit_on_reference_result: number;
+    information_tab: boolean;
+    icon: ButterImage;
+    title: string;
+    link: string;
+    link_text: string;
+    wysiwyg_content: boolean;
+    text_content: string;
+}
+
+export interface SidebarTabGroupI {
+    tabs: SidebarTabI[];
+}
+
+export interface SidebarInformationTabI {
+    icon: ButterImage;
+    title: string;
+    link: string;
+    link_text: string;
+}
+
+export interface SidebarButtonI {
+    sidebar_icon: ButterImage;
+    onClick: (arg: number) => void;
+    index: number;
+    activeIdx: number;
 }
