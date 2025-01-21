6786a06acf0e01e67e1132526df3478244525887
Alex Beciana <alex.beciana@gmail.com>
2025-01-21 15:35:01 -0700

    Created Nondiscrimination component

---
+++ components/_footer/Nondiscrimination/index.tsx
@@ -0,0 +1,27 @@
+import TextContent from "@/components/_styled/TextContent"
+import { NondiscriminationI } from '@/definitions/interfaces/_footer'
+import {
+    ColorE,
+    FontSizeE,
+    FontWeightE
+} from '@/definitions/enums'
+
+const Nondiscrimination: React.FC<NondiscriminationI> = ({
+    text
+}) => {
+    return (
+        <section className="py-20">
+            <TextContent
+                text='Notice of Nondiscrimination'
+                color={ColorE.DARKGREY}
+                fontWeight={FontWeightE.BOLD}
+                fontSize={FontSizeE.SM}
+            />
+            <div className='mt-2 pt-2 font-medium'>
+                {text}
+            </div>
+        </section>
+    )
+}
+
+export default Nondiscrimination
