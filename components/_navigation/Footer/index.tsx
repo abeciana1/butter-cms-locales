f72998f4cc8b055d8187623a6e0df426f49c2e08
Alex Beciana <alex.beciana@gmail.com>
2025-01-21 08:28:05 -0700

    Created initial render of footer content

--- components/_navigation/Footer/index.tsx
+++ components/_navigation/Footer/index.tsx
@@ -1,6 +1,11 @@
 import { use } from 'react'
+import { getNavMenu } from '@/lib/butter'
+import { headers } from 'next/headers';
 
 const Footer = () => {
+    const headersList = use(headers());
+    const isPreview = headersList.get("x-search-param")
+    const navContent = use(getNavMenu(isPreview as string, 'footer_menu', 'footer-menu'))
     return (
         <footer></footer>
     )
