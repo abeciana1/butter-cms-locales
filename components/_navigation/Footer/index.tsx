e9acb448a601552f417f733c58353129600fb6aa
Alex Beciana <alex.beciana@gmail.com>
2025-01-21 15:36:34 -0700

    Added content for first column

--- components/_navigation/Footer/index.tsx
+++ components/_navigation/Footer/index.tsx
@@ -2,6 +2,10 @@ import { use } from 'react'
 import { getNavMenu } from '@/lib/butter'
 import { headers } from 'next/headers';
 import Link from 'next/link'
+import PolicyLinkSection from '@/components/_footer/PolicyLinks'
+import Nondiscrimination from '@/components/_footer/Nondiscrimination'
+import TextContent from '@/components/_styled/TextContent';
+import { ColorE, FontSizeE, FontWeightE } from '@/definitions/enums';
 
 const Footer = () => {
     const headersList = use(headers());
@@ -20,7 +24,40 @@ const Footer = () => {
     } = navContent?.data?.fields
 
     return (
-        <footer className='bg-lightGrey mt-12 pt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'></footer>
+        <footer className='bg-lightGrey mt-12 pt-20 px-32'>
+            <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                <section>
+                    <Link
+                        href='/'
+                        className='text-blue hover:text-navy text-xl font-medium'
+                        title={medical_center_name}
+                    >
+                        {medical_center_name}
+                    </Link>
+                    <div className='space-y-1'>
+                        {address}
+                    </div>
+                    <div className='font-bold text-sm text-darkGrey'>
+                        {phone}
+                    </div>
+                    {(social_links && social_links?.length > 0) &&
+                        <></>
+                    }
+                    {(policy_links && policy_links?.length > 0) &&
+                        <PolicyLinkSection links={policy_links} />
+                    }
+                    <div className='mt-5'>
+                        <TextContent
+                            text={`Copyright ©${new Date().getFullYear()}. All rights reserved.`}
+                            fontSize={FontSizeE.SM}
+                            fontWeight={FontWeightE.MEDIUM}
+                            color={ColorE.DARKGREY}
+                        />
+                    </div>
+                </section>
+            </section>
+            <Nondiscrimination text={nondiscrimination_notice} />
+        </footer>
     )
 }
 
