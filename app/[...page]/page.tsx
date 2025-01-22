7e2b707abf9e6058811b23b4a181ea3048ab0255
Alex Beciana <alex.beciana@gmail.com>
2025-01-22 14:43:00 -0700

    Added in metadata generation function

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -1,10 +1,41 @@
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import ComponentRenderer from '@/components/ComponentRender'
 import { PageMarginWrapper } from '@/components/_layouts'
 import { headers } from 'next/headers';
 import { use } from 'react'
 import cx from 'classnames'
 import { getPageData } from '@/lib/butter'
+import { PageProps } from '@/definitions/interfaces/general'
+
+export const generateMetadata = async (
+    {searchParams}: PageProps
+  ): Promise<Metadata> => {
+    const resolvedSearchParams = await searchParams;
+    const headersList = await headers()
+    const path = headersList.get("x-pathname")
+    const isPreview =
+      (typeof resolvedSearchParams?.preview === 'string' &&
+      resolvedSearchParams.preview === '1') ? 'preview=1' : ''
+    const pageData = await getPageData(isPreview, path as string)
+    const {
+      seo
+    } = pageData?.data?.fields as any
+    console.log({ seo })
+    return {
+      title: seo?.title,
+      description: seo?.description,
+      openGraph: {
+        title: seo?.title,
+        description: seo?.description,
+        url: `https://butter-chs-demo-3j7c.vercel.app${path}`,
+        images: [
+          {
+            url: seo?.open_graph_image?.url
+          }
+        ]
+      }
+    }
+  }
 
 export default function DynamicPage() {
     const headersList = use(headers());
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
 import { use } from 'react'
 import { getPageData } from '@/lib/butter'
 import { headers } from 'next/headers';
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import ComponentRenderer from '@/components/ComponentRender'
 import { PageMarginWrapper } from '@/components/_layouts'
+import { PageProps } from '@/definitions/interfaces/general'
 
-// export const metadata: Metadata = () => {
-//   const data = pageDataFetch()
-//   console.log('data', data)
-// }
+export const generateMetadata = async (
+  {searchParams}: PageProps
+): Promise<Metadata> => {
+  const resolvedSearchParams = await searchParams;
+  const headersList = await headers()
+  const path = headersList.get("x-pathname")
+  const isPreview =
+    (typeof resolvedSearchParams?.preview === 'string' &&
+    resolvedSearchParams.preview === '1') ? 'preview=1' : ''
+  const pageData = await getPageData(isPreview, path as string)
+  const {
+    seo
+  } = pageData?.data?.fields as any
+  return {
+    title: seo?.title,
+    description: seo?.description,
+    openGraph: {
+      title: seo?.title,
+      description: seo?.description,
+      url: `https://butter-chs-demo-3j7c.vercel.app${path}`,
+      images: [
+        {
+          url: seo?.open_graph_image?.url
+        }
+      ]
+    }
+  }
+}
 
 export default function Home() {
   const headersList = use(headers());
