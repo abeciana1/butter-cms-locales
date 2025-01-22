1ef86cc147b88efb949b4b290dbea3279610c5e2
Alex Beciana <alex.beciana@gmail.com>
2025-01-22 12:27:41 -0700

    Converted to string

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set("x-pathname", request.nextUrl.pathname);
-    requestHeaders.set("x-search-param", request.nextUrl.searchParams)
+    requestHeaders.set("x-search-param", request.nextUrl.searchParams.toString())
     return NextResponse.next({
       request: {
         headers: requestHeaders,
