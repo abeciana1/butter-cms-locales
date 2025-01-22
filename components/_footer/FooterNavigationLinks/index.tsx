06d48dc46081ee60b004d38ef5c522b96c20ddbd
Alex Beciana <alex.beciana@gmail.com>
2025-01-21 19:51:26 -0700

    Created initial render of FooterNavigationLink

---
+++ components/_footer/FooterNavigationLinks/index.tsx
@@ -0,0 +1,11 @@
+import { FooterNavigationLinkI } from '@/definitions/interfaces/_footer'
+
+const FooterNavigationLink: React.FC<FooterNavigationLinkI> = ({
+    navLink
+}) => {
+    return (
+        <ul></ul>
+    )
+}
+
+export default FooterNavigationLink
