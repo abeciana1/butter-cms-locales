31e1ef6a13b31034f7a8c3b31346b195a99721bf
Alex Beciana <alex.beciana@gmail.com>
2025-01-21 19:51:42 -0700

    Created FooterNavigationLinkI and FooterDataI interfaces

--- definitions/interfaces/_footer/index.ts
+++ definitions/interfaces/_footer/index.ts
@@ -1,3 +1,19 @@
+import { NavigationLinkFieldI } from '@/definitions/interfaces/_navigation'
+
+export interface FooterDataI {
+    medical_center_name: string;
+    address: number;
+    phone: number;
+    nondiscrimination_notice: string;
+    policy_links: PolicyLinkI[];
+    social_links: SocialLinksI;
+    links: NavigationLinkFieldI[];
+}
+
+export interface FooterNavigationLinkI {
+    navLink: NavigationLinkFieldI;
+}
+
 export interface PolicyLinkI {
     link_label: string;
     link: string;
@@ -9,4 +25,17 @@ export interface PolicyLinkSectionI {
 
 export interface NondiscriminationI {
     text: string;
+}
+
+export interface SocialLinkI {
+    facebook: boolean;
+    facebook_username: string;
+    instagram: boolean;
+    instagram_username: string;
+    twitter: boolean;
+    twitter_username: string;
+}
+
+export interface SocialLinksI {
+    data: SocialLinkI;
 }
