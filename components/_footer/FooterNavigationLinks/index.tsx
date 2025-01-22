6408b0edfccbfb1db63c30757304ee8798a42b3f
Alex Beciana <alex.beciana@gmail.com>
2025-01-21 20:08:23 -0700

    Added navLink console log and child FooterNavigationSubLinks component

--- components/_footer/FooterNavigationLinks/index.tsx
+++ components/_footer/FooterNavigationLinks/index.tsx
@@ -1,8 +1,15 @@
 import { FooterNavigationLinkI } from '@/definitions/interfaces/_footer'
 
+const FooterNavigationSubLinks = () => {
+    return (
+        <></>
+    )
+}
+
 const FooterNavigationLink: React.FC<FooterNavigationLinkI> = ({
     navLink
 }) => {
+    console.log('navLink', navLink)
     return (
         <section></section>
     )
