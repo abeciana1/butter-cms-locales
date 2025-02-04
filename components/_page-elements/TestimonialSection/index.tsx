004909fa41eb5d47ab3318e813936c67cc81d6a6
Alex Beciana <alex.beciana@gmail.com>
2025-02-04 16:02:01 -0700

    Passed in properties and mapped over reviews data

--- components/_page-elements/TestimonialSection/index.tsx
+++ components/_page-elements/TestimonialSection/index.tsx
@@ -1,5 +1,6 @@
-import { TestimonialSectionI } from '@/definitions/interfaces/_section'
+import { TestimonialSectionI, TestimonialDataI } from '@/definitions/interfaces/_section'
 import { Heading1 } from '@/components/_styled/Heading'
+import TestimonialCard from '@/components/_styled/Testimonial'
 
 const TestimonialSection: React.FC<TestimonialSectionI> = ({
     heading,
@@ -9,7 +10,7 @@ const TestimonialSection: React.FC<TestimonialSectionI> = ({
 }) => {
     console.log('headingFontWeight', headingFontWeight)
     return (
-        <section className='mt-10'>
+        <section className='mt-10 pb-16 mx-5 border-b-2 border-shades-silver'>
             <div className='text-center'>
                 <Heading1
                     text={heading}
@@ -17,6 +18,24 @@ const TestimonialSection: React.FC<TestimonialSectionI> = ({
                     fontWeight={headingFontWeight?.font_weight}
                 />
             </div>
+            {(reviews && reviews?.length > 0) &&
+                <div>
+                    {reviews?.map((review: TestimonialDataI) => {
+                        return (
+                            <TestimonialCard
+                                key={review?.meta?.id}
+                                name={review?.name}
+                                quote={review?.quote}
+                                job_role={review?.job_role}
+                                company_name={review?.company_name}
+                                company_size={review?.company_size}
+                                headshot={review?.headshot}
+                                industry={review?.industry}
+                            />
+                        )
+                    })}
+                </div>
+            }
         </section>
     )
 }
