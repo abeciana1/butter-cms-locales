00693799bed71555916b7eac9df13723735417e8
Alex Beciana <alex.beciana@gmail.com>
2025-02-04 16:01:42 -0700

    Created initial render on TestimonialCard

---
+++ components/_styled/Testimonial/index.tsx
@@ -0,0 +1,18 @@
+import { TestimonialDataI } from '@/definitions/interfaces//_section'
+import React from 'react'
+
+const TestimonialCard: React.FC<TestimonialDataI> = ({
+    name,
+    quote,
+    job_role,
+    company_name,
+    company_size,
+    headshot,
+    industry
+}) => {
+    return (
+        <div></div>
+    )
+}
+
+export default TestimonialCard
