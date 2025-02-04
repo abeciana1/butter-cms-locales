1ef1d235eac692577eed1718e3908bc3e9c2977e
Alex Beciana <alex.beciana@gmail.com>
2025-02-04 11:52:53 -0700

    Added content to component

--- components/_page-elements/NavigationLinkButton/index.tsx
+++ components/_page-elements/NavigationLinkButton/index.tsx
@@ -1,7 +1,38 @@
-const NavigationLinkButton = (props: any) => {
-    console.log('props', props)
+'use client'
+import { NavigationLinkButtonI } from '@/definitions/interfaces/_navigation'
+import TextContent from '@/components/_styled/TextContent'
+import {
+    ColorE,
+    FontWeightE,
+    FontSizeE
+} from '@/definitions/enums'
+import Typeform from '@/components/_styled/Typeform'
+import { useRef } from 'react'
+import { GenericEmbed } from '@typeform/embed-react';
+
+const NavigationLinkButton: React.FC<NavigationLinkButtonI> = ({
+    label,
+    form,
+    formEmbed
+}) => {
+    // const buttonRef = useRef<HTMLButtonElement | null>(null)
+    const embedRef = useRef<GenericEmbed>(undefined)
+
+    const typeformOpenHandler = () => {
+        embedRef?.current?.open()
+    }
     return (
-        <></>
+        <>
+            <Typeform id={form?.id} embed={formEmbed} embedRef={embedRef} />
+            <button onClick={typeformOpenHandler}>
+                <TextContent
+                    text={label}
+                    fontSize={FontSizeE.XL}
+                    color={ColorE.SECONDARY}
+                    fontWeight={FontWeightE.MEDIUM}
+                />
+            </button>
+        </>
     )
 }
 
