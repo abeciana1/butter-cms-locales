7e2d9539ba214d9337fbc0b1c5da3bc0516c6733
Alex Beciana <alex.beciana@gmail.com>
2025-02-07 07:44:57 -0700

    Changed type

--- definitions/interfaces/_forms/index.ts
+++ definitions/interfaces/_forms/index.ts
@@ -3,6 +3,6 @@ import { GenericEmbed } from '@typeform/embed-react';
 
 export interface TypeformI {
     id: string;
-    embed: string;
+    embed: 'popup' | 'side-tab' | 'slider';
     embedRef?: RefObject<GenericEmbed | undefined>;
 }
