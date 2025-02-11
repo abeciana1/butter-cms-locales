18768ff1a236d0fc354c6ac868816ecfd427843e
Alex Beciana <alex.beciana@gmail.com>
2025-02-11 11:47:06 -0700

    Adjusted top alignment to be 50%

--- components/_page-elements/SliderHero/index.tsx
+++ components/_page-elements/SliderHero/index.tsx
@@ -20,36 +20,36 @@ const Hero: React.FC<HeroI> = ({
 }) => {
     return (
         <section className='relative flex items-center'>
-            <div
-                className={cx('absolute flex flex-row z-50 top-5 md:top-10 xl:top-20 lg:w-1/2 pl-16', {
-                    ['right-0']: content_on_right
-                })}
-            >
-                <div className=''>
-                    <Heading1
-                        text={headline}
-                        fontWeight={headline_font_weight?.font_weight}
-                        color={headline_color?.color}
-                    />
-                    <div className='mt-5'>
-                        <TextContent
-                            text={text_body}
-                            color={headline_color?.color}
-                            fontSize={FontSizeE.XL}
-                            fontWeight={FontWeightE.MEDIUM}
-                        />
-                    </div>
-                    <div className='mt-5'>
-                        <ButtonLink
-                            link={cta_button_link}
-                            linkText={cta_button_label}
-                            buttonColor={button_color?.color}
-                        />
-                    </div>
-                </div>
-                <div/>
-            </div>
             <div className='relative'>
+                <div
+                    className={cx('absolute flex flex-row z-50 top-[25%] lg:w-1/2 pl-16', {
+                        ['right-0']: content_on_right
+                    })}
+                >
+                    <div className=''>
+                        <Heading1
+                            text={headline}
+                            fontWeight={headline_font_weight?.font_weight}
+                            color={headline_color?.color}
+                        />
+                        <div className='mt-5'>
+                            <TextContent
+                                text={text_body}
+                                color={headline_color?.color}
+                                fontSize={FontSizeE.XL}
+                                fontWeight={FontWeightE.MEDIUM}
+                            />
+                        </div>
+                        <div className='mt-5'>
+                            <ButtonLink
+                                link={cta_button_link}
+                                linkText={cta_button_label}
+                                buttonColor={button_color?.color}
+                            />
+                        </div>
+                    </div>
+                    <div/>
+                </div>
                 <Image
                     src={background_image?.url}
                     alt={background_image.alt}
