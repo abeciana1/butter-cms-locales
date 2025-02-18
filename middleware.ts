14b3e1fef07231f641992af6fc055c9d3891f4c6
Alex Beciana <alex.beciana@gmail.com>
2025-02-18 11:26:28 -0700

    Removed console log

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    console.log('middleware request', request.nextUrl.pathname)
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set("x-pathname", request.nextUrl.pathname);
     requestHeaders.set("x-subdir", request.nextUrl.pathname.split('/')[1]);
