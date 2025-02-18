11aba8aeca459e9f55b331522d138c921a5b68c7
Alex Beciana <alex.beciana@gmail.com>
2025-02-18 11:25:58 -0700

    Added middleware for subdirs

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+    console.log('middleware request', request.nextUrl.pathname)
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set("x-pathname", request.nextUrl.pathname);
+    requestHeaders.set("x-subdir", request.nextUrl.pathname.split('/')[1]);
     requestHeaders.set("x-search-param", request.nextUrl.searchParams.toString())
     return NextResponse.next({
       request: {
