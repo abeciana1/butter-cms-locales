6d0b34342d6430ac4d48d2dddce1576b5b92f085
Alex Beciana <alex.beciana@gmail.com>
2025-02-19 09:48:37 -0700

    Added blog engine posts fetch and added heading styling

--- components/_page-elements/BlogEngineSection/index.tsx
+++ components/_page-elements/BlogEngineSection/index.tsx
@@ -1,15 +1,32 @@
 import { use } from 'react'
 import { BlogEngineSectionI } from '@/definitions/interfaces/_section'
+import { Heading1 } from '@/components/_styled/Heading'
+import { blogEnglinePostListFetch } from '@/lib/butter'
 
-const BlogEngineSection: React.FC<BlogEngineSectionI> = ({
+const BlogEngineSection: React.FC<BlogEngineSectionI> = async ({
     heading,
     headingFontColor,
     headingFontWeight,
     categoryFilter,
     limit
 }) => {
+    const blogPosts = await blogEnglinePostListFetch({
+        page: 1,
+        page_size: limit,
+        exclude_body: true,
+        category_slug: categoryFilter === 'all' ? '*' : categoryFilter
+    })
+    console.log('blogPosts', blogPosts)
     return (
-        <></>
+        <section className='mt-32 pb-16 mx-5'>
+            <div className='text-center'>
+                <Heading1
+                    text={heading}
+                    color={headingFontColor?.color}
+                    fontWeight={headingFontWeight?.font_weight}
+                />
+            </div>
+        </section>
     )
 }
 
