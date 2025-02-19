369283e06a009e7b3ade4d128428f9b137695095
Alex Beciana <alex.beciana@gmail.com>
2025-02-19 09:27:10 -0700

    Created initial render

---
+++ components/_page-elements/BlogEngineSection/index.tsx
@@ -0,0 +1,16 @@
+import { use } from 'react'
+import { BlogEngineSectionI } from '@/definitions/interfaces/_section'
+
+const BlogEngineSection: React.FC<BlogEngineSectionI> = ({
+    heading,
+    headingFontColor,
+    headingFontWeight,
+    categoryFilter,
+    limit
+}) => {
+    return (
+        <></>
+    )
+}
+
+export default BlogEngineSection
