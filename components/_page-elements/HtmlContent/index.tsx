3a150ab32c9cc63214931e7588114571bebd3b3b
Alex Beciana <alex.beciana@gmail.com>
2025-02-20 07:47:09 -0700

    Added allowable tag and attrs

--- components/_page-elements/HtmlContent/index.tsx
+++ components/_page-elements/HtmlContent/index.tsx
@@ -8,12 +8,15 @@ interface HTMLContentI {
 const HtmlContent: React.FC<HTMLContentI> = ({
     textContent
 }) => {
-    const sanitizedContent = DOMPurify.sanitize(textContent)
+    const sanitizedContent = DOMPurify.sanitize(textContent, {
+        ADD_TAGS: ['iframe', 'img'],
+        ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling', 'src'],
+    })
     return (
-        <div
-            className='html-content leading-relaxed'
-            dangerouslySetInnerHTML={{__html: sanitizedContent}}
-        />
+            <div
+                className='html-content leading-relaxed'
+                dangerouslySetInnerHTML={{__html: sanitizedContent}}
+            />
     )
 }
 
