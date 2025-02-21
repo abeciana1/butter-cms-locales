cf638c43de26bbe850136f87b21e31f2d8b9f4a6
Alex Beciana <alex.beciana@gmail.com>
2025-02-21 09:57:17 -0700

    Added locale header and added to args for page data fetch

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -23,10 +23,13 @@ export const generateMetadata = async (
     const resolvedSearchParams = await searchParams;
     const headersList = await headers()
     const path = headersList.get("x-pathname")
+    const locale = headersList.get("x-locale")
+    const subDir = headersList.get("x-subdir")
+    const pageType = subDir ? pageTypeLookup[subDir] : '*'
     const isPreview =
     (typeof resolvedSearchParams?.preview === 'string' &&
     resolvedSearchParams.preview === '1') ? 'preview=1' : ''
-    const pageData = await getPageData(isPreview, path as string)
+    const pageData = await getPageData(isPreview as string, path as string, pageType as string, locale as string)
     const {
         seo
     } = (pageData?.data?.fields ?? {}) as PageFields
@@ -67,7 +70,8 @@ export default function DynamicPage() {
     const path = headersList.get("x-pathname")
     const subDir = headersList.get("x-subdir")
     const pageType = subDir ? pageTypeLookup[subDir] : '*'
-    const pageContent = use(getPageData(isPreview as string, path as string, pageType as string))
+    const locale = headersList.get("x-locale")
+    const pageContent = use(getPageData(isPreview as string, path as string, pageType as string, locale as string))
     const {
         sidebar,
         body
