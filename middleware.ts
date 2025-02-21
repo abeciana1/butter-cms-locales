86127e63a544ad83e9fe712343d2bc72a83f5122
Alex Beciana <alex.beciana@gmail.com>
2025-02-21 09:27:50 -0700

    Added locale header

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log('locale test', request.nextUrl.pathname.split('/'))
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-pathname", request.nextUrl.pathname);
-  // requestHeaders.set("x-locale", )
-  requestHeaders.set("x-subdir", request.nextUrl.pathname.split('/')[1]);
+  requestHeaders.set("x-locale", request.nextUrl.pathname.split('/')[1])
+  requestHeaders.set("x-subdir", request.nextUrl.pathname.split('/')[2]);
   requestHeaders.set("x-search-param", request.nextUrl.searchParams.toString())
   return NextResponse.next({
     request: {
