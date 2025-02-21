0e7cea4b2598610df0f0700af244831779a594db
Alex Beciana <alex.beciana@gmail.com>
2025-02-21 09:18:05 -0700

    Fixed indent and added console log

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,15 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    const requestHeaders = new Headers(request.headers);
-    requestHeaders.set("x-pathname", request.nextUrl.pathname);
-    requestHeaders.set("x-subdir", request.nextUrl.pathname.split('/')[1]);
-    requestHeaders.set("x-search-param", request.nextUrl.searchParams.toString())
-    return NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
-  }
+  console.log('locale test', request.nextUrl.pathname.split('/'))
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-pathname", request.nextUrl.pathname);
+  // requestHeaders.set("x-locale", )
+  requestHeaders.set("x-subdir", request.nextUrl.pathname.split('/')[1]);
+  requestHeaders.set("x-search-param", request.nextUrl.searchParams.toString())
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
