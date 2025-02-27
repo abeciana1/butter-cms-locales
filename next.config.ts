cb5e445093f7a851384a5eb0a817bcc57169b220
Alex Beciana <alex.beciana@gmail.com>
2025-02-27 09:43:30 -0700

    Removed locale routing

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'fr', 'de', 'es']
-  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
