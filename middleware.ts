a927ada3175cfaca8b3f05eee4bde2bbf228c50a
Alex Beciana <alex.beciana@gmail.com>
2025-02-27 09:41:49 -0700

    Deconstructed request

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,13 @@ import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-pathname", request.nextUrl.pathname);
-  requestHeaders.set("x-locale", request.nextUrl.pathname.split('/')[1])
-  requestHeaders.set("x-subdir", request.nextUrl.pathname.split('/')[2]);
-  requestHeaders.set("x-search-param", request.nextUrl.searchParams.toString())
+  const { nextUrl } = request;
+
+  requestHeaders.set("x-pathname", nextUrl.pathname);
+  requestHeaders.set("x-locale", nextUrl.pathname.split('/')[1])
+  requestHeaders.set("x-subdir", nextUrl.pathname.split('/')[2]);
+  requestHeaders.set("x-search-param", nextUrl.searchParams.toString())
+
   return NextResponse.next({
     request: {
       headers: requestHeaders,
